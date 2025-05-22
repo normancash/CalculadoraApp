64877af9e9169d482c513061107a8db1f856fb85
jadg-13 <jadg13@hotmail.com>
2025-05-21 18:52:42 -0600

    tercer commit

--- AplicationDemo/app/src/main/java/com/uam/aplicationdemo/composable/CalculatorScreen.kt
+++ AplicationDemo/app/src/main/java/com/uam/aplicationdemo/composable/CalculatorScreen.kt
@@ -1,5 +1,7 @@
 package com.uam.aplicationdemo.composable
 
+import android.annotation.SuppressLint
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -12,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -27,17 +30,30 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.uam.aplicationdemo.tools.evaluate
 
+@SuppressLint("DefaultLocale")
+fun evaluador(exp:String):String {
+    return try {
+        val eval = evaluate(exp)
+        if (eval % 1.0 == 0.0) eval.toInt().toString()
+        else String.format("%.4f",eval)
+    }
+    catch(e:Exception) {
+        "error"
+    }
+}
+
 @Composable
 fun CalculatorScreen() {
     var expression by rememberSaveable { mutableStateOf("") }
     var result by rememberSaveable { mutableStateOf("") }
     val buttons = listOf(
-        listOf("C","(",")","÷"),
-        listOf("7","8","9","*"),
-        listOf("4","5","6","-"),
-        listOf("1","2","3","+"),
-        listOf("0",".","=","")
+        listOf("C","(",")","<-"),
+        listOf("7","8","9","÷"),
+        listOf("4","5","6","*"),
+        listOf("1","2","3","-"),
+        listOf("0",".","=","+")
     )
+
 
     Column(
         modifier = Modifier.fillMaxSize()
@@ -66,11 +82,14 @@ fun CalculatorScreen() {
                        CalculatorButton(symbol = button){
                            when(button) {
                                "=" -> {
-                                   result = evaluate(expression).toString()
+                                   result = evaluador(expression)
                                }
                                "C" -> {
                                    expression = ""
                                    result = ""
+                               }
+                               "<-" -> {
+                                   expression = expression.dropLast(1)
                                }
                                else -> {
                                    expression += button
@@ -89,11 +108,18 @@ fun CalculatorScreen() {
 
 @Composable
 fun CalculatorButton(symbol: String,onClick : ()-> Unit) {
+    val backgroudcolor by animateColorAsState(
+        targetValue = when (symbol) {
+            "C" -> Color(0xFFFF6B6B)
+            "=","+","-","*","÷" -> Color(0xFF4D96FF)
+            else -> Color(0xFFE0E0E0)
+        },label = "buttonColor"
+    )
     Box(
         contentAlignment = Alignment.Center,
         modifier = Modifier.padding(8.dp)
             .size(80.dp)
-            .background(Color.Gray,RoundedCornerShape(20.dp))
+            .background(backgroudcolor,RoundedCornerShape(20.dp))
             .clickable { onClick() }
     ){
         Text(text = symbol,
